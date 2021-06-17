e1e79c212099d30ccc364de1f06ba79b9e033180
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-17 20:33:51 -0300

    chore: setup styles import swiper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
