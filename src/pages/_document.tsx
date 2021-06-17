ecc1817cc5e404cf920138b88baa2846429108a7
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-17 13:38:50 -0300

    chore: setup external font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <meta name="theme-color" content="#6A37FF" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
