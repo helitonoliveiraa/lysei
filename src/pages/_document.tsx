d3806e3dca56058eac620a4025eebefa76c1f22f
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-18 22:10:08 -0300

    style: setup leaflet css style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,13 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+            integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+            crossOrigin=""
+          />
         </Head>
 
         <body>
