0cec6fc07398662b63075b518bc5df38e35fa127
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-20 19:53:37 -0300

    chore: add new type

--- src/@types/modules.d.ts
+++ src/@types/modules.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     REACT_APP_MAPBOX_TOKEN: string;
+    API_ENDPOINT: string;
   }
 }
