ecbb4b6a0a1e795608bd6eec8eb2232d349052b6
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-20 10:45:54 -0300

    refactor: chande input style based on path route

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,6 +15,7 @@ import {
   FormLabel,
 } from '@chakra-ui/react';
 import { FieldError } from 'react-hook-form';
+import { useRouter } from 'next/router';
 
 type InputProps = ChakraInputProps & {
   icon: ElementType;
@@ -27,6 +28,9 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
   ref: ForwardedRef<HTMLInputElement>,
 ): JSX.Element => {
   const [isFocused, setIsFocused] = useState(false);
+
+  const { asPath } = useRouter();
+
   function handleInputFocus() {
     setIsFocused(true);
   }
@@ -36,52 +40,105 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
   }
 
   return (
-    <FormControl isInvalid={!!error}>
-      <Flex width="100%">
-        <FormLabel
-          htmlFor={name}
-          w="50px"
-          h="50px"
-          mr="0"
-          border={isFocused || !!error ? '2px solid' : '1px solid'}
-          borderRight="none"
-          borderLeftRadius="8px"
-          borderColor={
-            isFocused ? 'purple.600' : error ? 'red.400' : 'gray.200'
-          }
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Icon
-            as={icon}
-            fontSize="24"
-            color={isFocused ? 'purple.600' : error ? 'red.400' : 'gray.200'}
-            display="block"
-          />
-        </FormLabel>
-        <ChakraInput
-          ref={ref}
-          id={name}
-          name={name}
-          flex="1"
-          h="50px"
-          borderLeftRadius="none"
-          borderRightRadius="8px"
-          focusBorderColor="purple.600"
-          color="gray.400"
-          _placeholder={{
-            fontSize: 'md',
-            color: 'gray.300',
-          }}
-          onBlur={handleInputBlur}
-          onFocus={handleInputFocus}
-          {...rest}
-        />
-      </Flex>
+    <>
+      {asPath === '/' ? (
+        <FormControl isInvalid={!!error}>
+          <Flex width="100%">
+            <FormLabel
+              htmlFor={name}
+              w="50px"
+              h="50px"
+              mr="0"
+              borderRight="2px solid"
+              borderRightColor="purple.600"
+              borderLeftRadius="8px"
+              bgColor="gray.100"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Icon
+                as={icon}
+                fontSize="24"
+                color="purple.600"
+                display="block"
+              />
+            </FormLabel>
+            <ChakraInput
+              ref={ref}
+              id={name}
+              name={name}
+              flex="1"
+              h="50px"
+              borderLeftRadius="none"
+              borderRightRadius="8px"
+              focusBorderColor="purple.600"
+              bgColor="gray.100"
+              color="gray.400"
+              _placeholder={{
+                fontSize: 'md',
+                color: 'gray.300',
+              }}
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              {...rest}
+            />
+          </Flex>
 
-      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
-    </FormControl>
+          {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+        </FormControl>
+      ) : (
+        <FormControl isInvalid={!!error}>
+          <Flex width="100%">
+            <FormLabel
+              htmlFor={name}
+              w="50px"
+              h="50px"
+              mr="0"
+              border={isFocused || !!error ? '2px solid' : '1px solid'}
+              borderRight={asPath === '/' ? '1px solid' : 'none'}
+              borderRightColor="purple.600"
+              borderLeftRadius="8px"
+              borderColor={
+                isFocused ? 'purple.600' : error ? 'red.400' : 'gray.200'
+              }
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Icon
+                as={icon}
+                fontSize="24"
+                color={
+                  isFocused ? 'purple.600' : error ? 'red.400' : 'gray.200'
+                }
+                display="block"
+              />
+            </FormLabel>
+            <ChakraInput
+              ref={ref}
+              id={name}
+              name={name}
+              flex="1"
+              h="50px"
+              borderLeftRadius="none"
+              borderRightRadius="8px"
+              focusBorderColor="purple.600"
+              color="gray.400"
+              _placeholder={{
+                fontSize: 'md',
+                color: 'gray.300',
+              }}
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              {...rest}
+            />
+          </Flex>
+
+          {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+        </FormControl>
+      )}
+    </>
   );
 };
 
