64470f1563c0b58836b4831a9c42d4ea6b14a0f7
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-20 19:51:54 -0300

    chore: setup auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import { ModalContextProvider } from '../contexts/ModalContext';
+import { AuthProvider } from '../contexts/AuthContext';
 
 import 'swiper/swiper.min.css';
 import 'swiper/components/navigation/navigation.min.css';
@@ -9,9 +11,11 @@ import 'swiper/components/pagination/pagination.min.css';
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
-      <ModalContextProvider>
-        <Component {...pageProps} />
-      </ModalContextProvider>
+      <AuthProvider>
+        <ModalContextProvider>
+          <Component {...pageProps} />
+        </ModalContextProvider>
+      </AuthProvider>
     </ChakraProvider>
   );
 }
